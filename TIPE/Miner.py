4cce4c606d42e17e20aa4fb5e39a3c91c05dffd5
DucRaPL <clash.game10@gmail.com>
2021-05-11 22:42:03 +0200

    hlhl

--- TIPE/Miner.py
+++ TIPE/Miner.py
@@ -2,12 +2,7 @@ class Miner:
     blockchain = ""
     
     def __init__(self):
-<<<<<<< HEAD
         print "MODIF43T535353TTT"
-=======
-        print "MODIF"
-        print "DQZDIUQZDIZ"
->>>>>>> ca0b07a2137ae970cbe324d9ff753349772cf206
 
 
 
