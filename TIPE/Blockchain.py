33556e3c94fe4eda0d11b5a01b9a64103bfb5086
DucRaPL <clash.game10@gmail.com>
2021-05-28 20:27:22 +0200

    a

--- TIPE/Blockchain.py
+++ TIPE/Blockchain.py
@@ -5,3 +5,40 @@ class Blockchain:
         # Contient différentes alternatives de blockchain 
         # On va mettre toutes la blockchain dedans pour faciliter les comparaisons de taille
         self.alternateFollowingChains = [[]]   
+    
+    def chainUpdate(self) :
+        compteurFirst = 0
+        compteurSecond = 0
+        maxLength = 0
+        pos = 0
+        for (i,val) in enumerate(self.alternateFollowingChains) :
+            if len(val) > maxLength :
+                pos = i 
+                compteurFirst = 0
+                maxLength = len(val)
+            if len(val) == maxLength :
+                compteurFirst += 1
+                if compteurSecond < len(val) :
+                    compteurSecond = len(val)
+        if compteurFirst == 0 and compteurFirst > (compteurSecond + 5) :
+            self.validBlocks += self.alternateFollowingChains[:-5]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
