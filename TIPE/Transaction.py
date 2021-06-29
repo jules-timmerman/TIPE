402d9cd53d7fa860f87b81160f10164afdf905f1
DucRaPL <clash.game10@gmail.com>
2021-06-29 18:17:20 +0200

    T

--- TIPE/Transaction.py
+++ TIPE/Transaction.py
@@ -3,20 +3,24 @@ from hashlib import sha256
 class Transaction:
     
 
-    def __init__(self, personId, maladieId, newDate, clientId,signature = -1):
+    def __init__(self, personId, maladieId, newDate, clientId,privateKey):
         self.personId = personId    # id de la personne liée à la transaction
         self.maladieId = maladieId  # id de la maladie liée à la transaction
         self.newDate = newDate      # date à ajouter à la personne (que ce soit attrapée ou perdue)
         self.clientId = clientId    # nom de la personne qui a envoye la transaction
         
-        if signature == -1 :
-            s = ""
-            s += str(self.personId) + "|"
-            s += str(self.maladieId) + "|"
-            s += str(self.newDate) + "|"
-            s += str(self.clientId) 
-            signature = int.from_bytes(sha256(bytes(s, 'utf-8')).digest(), byteorder='big')
-            self.signature = signature  # Signature de la transaction
+        
+        s = ""
+        s += str(self.personId) + "|"
+        s += str(self.maladieId) + "|"
+        s += str(self.newDate) + "|"
+        s += str(self.clientId) 
+            
+        hash = int.from_bytes(sha256(bytes(s, 'utf-8')).digest(), byteorder='big')
+        
+        signature = pow(hash, privateKey[1], privateKey[0])
+
+        self.signature = signature  # Signature de la transaction
                                     # Ici par l'hopital et donc fait avec la clé privée
         
     
@@ -53,10 +57,10 @@ class Transaction:
 
         f = open("listeHopital.txt", "r")
         g = f.getLines()
-        h = g[clientId].split("%")
+        h = g[self.clientId].split("%")
 
         hash = int.from_bytes(sha256(bytes(s, 'utf-8')).digest(), byteorder='big')
-        hashFromSignature = pow(signature, int(h[1]),int(h[0]))
+        hashFromSignature = pow(self.signature, int(h[1]),int(h[0]))
 
         f.close()
 
