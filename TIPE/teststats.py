e980b77ae4714a2129e2e734850664856c247b14
snoopinou30 <jules.snoopi@live.fr>
2021-11-16 17:06:08 +0100

    etete

--- TIPE/teststats.py
+++ TIPE/teststats.py
@@ -3,21 +3,32 @@ from Block import Block
 from Transaction import Transaction
 import time
 from Crypto.PublicKey import RSA
+import random
+
+
+
+keyPair = RSA.generate(bits=1024)
+
+clepublique = [keyPair.n, keyPair.e]
+
+cleprivee = [keyPair.n , keyPair.d ]
+
+
 
 def hash(sum, pow): # Sum en str et pow un nombre
      s = bin(int.from_bytes(sha256(bytes(sum + str(pow), 'utf-8')).digest(), byteorder = "big"))[2:]
      s = (256-len(s))*'0' + s
      return s
 
-t1 = Transaction(1,2,"10/02/2003",3)
-t2 = Transaction(2,4,"10/02/1997",3)
-t3 = Transaction(5,4,"10/06/1997",5)
-t4 = Transaction(5,3,"01/06/1997",5)
-t5 = Transaction(5,3,"01/06/2007",5)
+t1 = Transaction(1,2,"10/02/2003",3,cleprivee)
+t2 = Transaction(2,4,"10/02/1997",3,cleprivee)
+t3 = Transaction(5,4,"10/06/1997",5,cleprivee)
+t4 = Transaction(5,3,"01/06/1997",5,cleprivee)
+t5 = Transaction(2,1,"01/06/2011",9,cleprivee)
 
 
 
-b = Block(1,"bonjour",)
+b = Block(1,"000110101010101001011111",[t1,t2,t3,t4,t5])
 
 N = 10**6
 
@@ -48,20 +59,23 @@ Nz = 7
 #    t = t2 - t1
 #    print(t)
 
+b.hashBlockWithPOW(100)
 
-for i in range(20):
+
+bool = True
+
+
+for k in range(40) :
+    i = random.randint(0,10**Nz)
+
     t1 = time.time()
     bool = True
-    i = 0
     while bool :
+
         h = b.hashBlockWithPOW(i)
         if h[0:Nz] == Nz*"0" :
+            t2 = time.time()
+            print(t2-t1)
             bool = False
         i += 1
-    print(i-1)
-    bool = True      
-    t2 = time.time()
 
-
-    t = t2 - t1
-    print(t)
